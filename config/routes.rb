0cae28d074d7e1bf8f1d3b24c6fb3765d2fddba8
Pedro Dourado <pdourado367@gmail.com>
2020-12-04 13:48:00 -0300

    add views and controller and model

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :backoffice do
     resources :pages, except: :show 
+    resources :categories, except: :show 
   end
 end
