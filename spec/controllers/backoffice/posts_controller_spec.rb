2e432821e70aaa18a834228723c75d87ff5a9f0f
Werter Meira <wertermeira@gmail.com>
2020-12-07 14:42:32 -0300

    create test

--- spec/controllers/backoffice/posts_controller_spec.rb
+++ spec/controllers/backoffice/posts_controller_spec.rb
@@ -16,6 +16,40 @@ RSpec.describe Backoffice::PostsController, type: :controller do
     }
   end
 
+  describe '#index' do
+    context 'when is success' do
+      it 'returns http success' do
+        get :index
+        expect(response).to have_http_status(:success)
+      end
+    end
+  end
+
+  describe '#show' do
+    it 'returns http success' do
+      get :show, params: { id: post.slug }
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'returns http nof found' do
+      get :show, params: { id: 'notfoundid' }
+      expect(response).to have_http_status(:not_found)
+    end
+
+    it 'returns http nof found (not published)' do
+      post.update(active: false)
+      get :show, params: { id: post.slug }
+      expect(response).to have_http_status(:not_found)
+    end
+  end
+
+  describe '#new' do
+    it 'returns http success' do
+      get :new
+      expect(response).to have_http_status(:success)
+    end
+  end
+
   describe '#create' do
     context 'when created' do
       it 'created' do
@@ -39,6 +73,46 @@ RSpec.describe Backoffice::PostsController, type: :controller do
       attributes[:name] = ''
       post :create, params: { post: attributes }
       expect(response).to render_template(:new)
+    end
+  end
+
+  describe '#edit' do
+    it 'returns http success' do
+      get :edit, params: { id: post.slug }
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe '#update' do
+    context 'when accepted' do
+      before { put :update, params: { id: post.slug, post: attributes } }
+
+      it 'change name' do
+        expect(post.reload.name).to eq(name)
+      end
+
+      it 'redirect to posts' do
+        expect(response).to redirect_to(backoffice_posts_path)
+      end
+    end
+
+    it 'update is fail' do
+      attributes[:name] = ''
+      put :update, params: { id: post.slug, post: attributes }
+      expect(response).to render_template(:edit)
+    end
+  end
+
+  describe '#destroy' do
+    it 'destroy is redirect_to' do
+      delete :destroy, params: { id: post.slug }
+      expect(response).to redirect_to(backoffice_posts_path)
+    end
+
+    it 'destroy is deleted' do
+      expect do
+        delete :destroy, params: { id: post.slug }
+      end.to change(Post, :count).by(0)
     end
   end
 end
